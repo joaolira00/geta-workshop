48a90e8b519a998e2c680fa709059541b8a4f0ff
joaolira00 <joao134.jol@gmail.com>
2024-10-09 15:01:46 -0300

    Moved Patient management methods to new file.

--- Controllers/HospitalController.cs
+++ Controllers/HospitalController.cs
@@ -57,58 +57,6 @@ namespace MedGETA.Controllers
             return hospitalDTO;
         }
 
-        [HttpGet("listAllPatientsInHospital/{id}")]
-        public ActionResult<PatientModel> GetPatientsInHospital(Guid id)
-        {
-            Hospital? hospital = _context.Hospitals?
-                                            .Include(h => h.Patients)
-                                            .FirstOrDefault(h => h.Id == id);
-
-            if (hospital == null)
-            {
-                return NotFound("No hospital found with this Id.");
-            }
-
-            List<PatientModel>? patientDTO = hospital.Patients?.Select(p => new PatientModel
-            {
-                Id = p.Id,
-                Name = p.Name,
-                Email = p.Email
-            }).ToList();
-
-            return Ok(patientDTO);
-        }
-
-        [HttpPost("AddPatientToHospital/{hospitalName}")]
-        public ActionResult<Patient> AddPatientToHospital(string hospitalName, [FromBody] List<string> patientList)
-        {
-            Hospital? _hospital = _context.Hospitals?
-                                            .Include(h => h.Patients)
-                                            .FirstOrDefault(h => h.Name == hospitalName);
-
-            if (_hospital is null)
-            {
-                return BadRequest("Incomplete information about the patient, try again.");
-            }
-
-            List<Patient>? _patientToAdd = _context.Patients?.Where(p => patientList.Contains(p.Name!)).ToList();
-
-            if (_patientToAdd is null)
-            {
-                return NotFound("No patient selected.");
-            }
-
-            foreach(Patient patient in _patientToAdd)
-            {
-                _hospital.Patients?.Add(patient);
-            }
-
-            _context.SaveChanges();
-
-            return Ok("Patient registered succesfully.");
-
-        }
-
         [HttpPost("createNewHospital")]
         public ActionResult<Hospital> CreateNewHospital([FromBody] Hospital? _hospital)
         {
