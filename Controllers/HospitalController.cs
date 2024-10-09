172fdcace37a2813851dfcb78adb13b64ba1129a
joaolira00 <joao134.jol@gmail.com>
2024-10-09 10:41:16 -0300

    Added methods for manipulation of patients in the Hospital collections.

--- Controllers/HospitalController.cs
+++ Controllers/HospitalController.cs
@@ -1,7 +1,10 @@
 using MedGETA.Context;
 using MedGETA.DTO.Hospital;
+using MedGETA.DTO.Patient;
 using MedGETA.Hospitals;
+using MedGETA.Patients;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MedGETA.Controllers
 {
@@ -54,6 +57,58 @@ namespace MedGETA.Controllers
             return hospitalDTO;
         }
 
+        [HttpGet("listAllPatientsInHospital/{id}")]
+        public ActionResult<PatientModel> GetPatientsInHospital(Guid id)
+        {
+            Hospital? hospital = _context.Hospitals?
+                                            .Include(h => h.Patients)
+                                            .FirstOrDefault(h => h.Id == id);
+
+            if (hospital == null)
+            {
+                return NotFound("No hospital found with this Id.");
+            }
+
+            List<PatientModel>? patientDTO = hospital.Patients?.Select(p => new PatientModel
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Email = p.Email
+            }).ToList();
+
+            return Ok(patientDTO);
+        }
+
+        [HttpPost("AddPatientToHospital/{hospitalName}")]
+        public ActionResult<Patient> AddPatientToHospital(string hospitalName, [FromBody] List<string> patientList)
+        {
+            Hospital? _hospital = _context.Hospitals?
+                                            .Include(h => h.Patients)
+                                            .FirstOrDefault(h => h.Name == hospitalName);
+
+            if (_hospital is null)
+            {
+                return BadRequest("Incomplete information about the patient, try again.");
+            }
+
+            List<Patient>? _patientToAdd = _context.Patients?.Where(p => patientList.Contains(p.Name!)).ToList();
+
+            if (_patientToAdd is null)
+            {
+                return NotFound("No patient selected.");
+            }
+
+            foreach(Patient patient in _patientToAdd)
+            {
+                _hospital.Patients?.Add(patient);
+            }
+
+            _context.SaveChanges();
+
+            return Ok("Patient registered succesfully.");
+
+        }
+
         [HttpPost("createNewHospital")]
         public ActionResult<Hospital> CreateNewHospital([FromBody] Hospital? _hospital)
         {
@@ -86,6 +141,35 @@ namespace MedGETA.Controllers
             _context.SaveChanges();
 
             return Ok("Hospital deleted succesfully!");
+        }
+
+        [HttpDelete("RemoveFromHospital/{hospitalName}")]
+        public ActionResult<Patient> RemoveFromHospital(string hospitalName, [FromBody] List<Guid> patientList)
+        {
+            Hospital? _hospital = _context.Hospitals?
+                                        .Include(h => h.Patients)
+                                        .FirstOrDefault(h => h.Name == hospitalName);
+
+            if (_hospital is null)
+            {
+                return NotFound("No hospitals found with this Id.");
+            }
+
+            List<Patient>? patientsToBeDeleted = _hospital?.Patients?.Where(p => patientList.Contains(p.Id)).ToList();
+
+            if (patientsToBeDeleted is null || patientsToBeDeleted.Count == 0)
+            {
+                return NotFound("No patients found for removal.");
+            }
+
+            foreach(Patient patient in patientsToBeDeleted)
+            {
+                _hospital?.Patients?.Remove(patient);
+            }
+
+            _context.SaveChanges();
+
+            return Ok(patientsToBeDeleted);
         }
     }
 }
